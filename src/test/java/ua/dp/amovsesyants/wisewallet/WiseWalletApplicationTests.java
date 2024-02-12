c1b6cc72346de716ce33f7b8938aef714534c47f
Artem Movsesyants <artmih@gmail.com>
2024-02-12 12:42:26 +0200

    first commit

---
+++ src/test/java/ua/dp/amovsesyants/wisewallet/WiseWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.dp.amovsesyants.wisewallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiseWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
